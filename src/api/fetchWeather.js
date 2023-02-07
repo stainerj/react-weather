0f363a982bfd0e8190dd3da4a9929de4459e8c59
stainerj <jstainer@outlook.com>
2023-02-07 20:27:08 +0000

    commit 1. api-key removed.

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = 'api=key goes here';
+const API_KEY = 'api-key goes here';
 
 export const fetchWeather = async (query) => {
     const {data} = await axios.get(URL, {
